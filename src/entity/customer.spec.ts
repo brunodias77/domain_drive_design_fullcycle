67ce16bc2bd569bea151ec90b7afe0818358d707
brunodias77 <brunohdias95@gmail.com>
2023-04-12 13:04:59 -0300

    refactor: new test in customer

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -2,21 +2,21 @@ import Address from "./address";
 import Customer from "./customer";
 
 describe("Teste unitarios do cliente", () => {
-  it("should throw error when id is empty", () => {
+  it("deve lançar um erro quando o id está vazio", () => {
     expect(() => new Customer("", "John")).toThrowError("Id is required");
   });
 
-  it("should throw error when name is empty", () => {
+  it("deve lançar um erro quando o nome está vazio", () => {
     expect(() => new Customer("123", "")).toThrowError("Name is required");
   });
 
-  it("should change name", () => {
+  it("deveria mudar de nome", () => {
     const customer = new Customer("123", "Bruno Dias");
     customer.changeName("John");
     expect(customer.name).toBe("John");
   });
 
-  it("should activate customer", () => {
+  it("deve ativar o cliente", () => {
     const customer = new Customer("123", "Bruno Dias");
     const address = new Address(
       "Rosa Iachel Mazetto",
@@ -29,16 +29,27 @@ describe("Teste unitarios do cliente", () => {
     expect(customer.isActive()).toBe(true);
   });
 
-  it("should throw error when address id undefined when you activate a customer", () => {
+  it("deve lançar um erro quando o id do endereço é indefinido quando você ativa um cliente", () => {
     expect(() => {
       const customer = new Customer("123", "Bruno Dias");
       customer.activate();
     }).toThrowError("Address is mandatory to activate a customer");
   });
 
-  it("should deactivate customer", () => {
+  it("deveria desativar o cliente", () => {
     const customer = new Customer("123", "Bruno Dias");
     customer.deactivate();
     expect(customer.isActive()).toBe(false);
+  });
+
+  it("deve adicionar pontos de fidelidade", () => {
+    const customer = new Customer("1", "Customer 1");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(20);
   });
 });
