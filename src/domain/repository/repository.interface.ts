62447829a7618dd551800270a7b21dd19b566377
brunodias77 <brunohdias95@gmail.com>
2023-04-13 09:16:08 -0300

    feat: add repository interface

---
+++ src/domain/repository/repository.interface.ts
@@ -0,0 +1,6 @@
+export default interface RepositoryInterface<T> {
+  create(item: T): Promise<void>;
+  update(item: T): Promise<void>;
+  find(id: string): Promise<T>;
+  findAll(): Promise<T[]>;
+}
